61e9377b0d76e07f49f81c5dde804de94e578c87
Unknown <fofonso_97@me.com>
2018-03-30 11:52:14 -0700

    Se agrega combobox de municipios

--- src/combos/ComboBoxEdoCdMun.java
+++ src/combos/ComboBoxEdoCdMun.java
@@ -1,30 +1,40 @@
 package combos;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ComboBoxEdoCdMun extends JPanel {
+public class ComboBoxEdoCdMun extends JPanel implements ActionListener {
 	
 	private JComboBox<String> comBoxEstados;
+	private JComboBox<String> comBoxMunicipios;
+	
 	private ArchivoEstados archivoEstados;
+	private ArchivoMunicipios archivoMunicipios;
 
 	public ComboBoxEdoCdMun() throws Exception {
 		crearInterfaz();
+		crearEventos();
 		setVisible(true);
 	}
 	
 	public void crearInterfaz() throws Exception {
-		setLayout(new GridLayout(1,0));
+		setLayout(new GridLayout(3,0));
 		
-		add(new JLabel("Estados:"));
-		crearComboBoxEstados();	
+		inicializarArchivos();
+		
+		crearComboBoxEstados();
+		crearComboBoxMunicipios();
 	}
 	
 	public void crearComboBoxEstados() throws Exception {		
+		add(new JLabel("Estados:"));
 		String[] elementosComboBox = crearElementosComboBoxEstados();		
 		comBoxEstados = new JComboBox<String>(elementosComboBox);
 		add(comBoxEstados);
@@ -39,6 +49,40 @@ public class ComboBoxEdoCdMun extends JPanel {
 			elementos[i] = listaEstados.get(i-1).getNombreEstado();
 		}
 		return elementos;
+	}
+	
+	public void crearComboBoxMunicipios() throws Exception {		
+		add(new JLabel("Municipios:"));
+		String[] elementosComboBox = crearElementosComboBoxMunicipios();		
+		comBoxMunicipios = new JComboBox<String>(elementosComboBox);
+		comBoxMunicipios.setEnabled(false);
+		add(comBoxMunicipios);
+	}
+	
+	public String[] crearElementosComboBoxMunicipios() throws Exception  {
+		ArrayList<Municipio> listaMunicipios = archivoMunicipios.leerMunicipios();
+		String[] elementos = new String[listaMunicipios.size()+1];
+		elementos[0] = "Seleccionar";
+		for(int i = 1; i < elementos.length; i++) {
+			elementos[i] = listaMunicipios.get(i-1).getNombreMunicipio();
+		}
+		return elementos;
+	}
+	
+	public void inicializarArchivos() throws FileNotFoundException {
+		archivoEstados = new ArchivoEstados();
+		archivoMunicipios = new ArchivoMunicipios();
+	}
+	
+	public void crearEventos() {
+		comBoxEstados.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == comBoxEstados) {
+			System.out.println("Estado seleccionado");
+		}
 	}
 	
 }
---
+++ src/combos/Municipio.java
@@ -0,0 +1,53 @@
+package combos;
+
+public class Municipio {
+
+	private Integer estadoId;
+	private Integer municipioId;
+	private String nombreMunicipio;
+	
+	public Municipio() {
+		super();
+		this.estadoId = 0;
+		this.municipioId = 0;
+		this.nombreMunicipio = null;
+	}
+	
+	public Municipio(int municipioId, String nombreMunicipio, int estadoId) {
+		super();
+		this.municipioId = municipioId;
+		this.nombreMunicipio = nombreMunicipio;
+		this.estadoId = estadoId;
+	}
+
+	public int getMunicipioId() {
+		return municipioId;
+	}
+
+	public void setMunicipioId(Integer municipioId) {
+		this.municipioId = municipioId;
+	}
+
+	public String getNombreMunicipio() {
+		return nombreMunicipio;
+	}
+
+	public void setNombreMunicipio(String nombreMunicipio) {
+		this.nombreMunicipio = nombreMunicipio;
+	}
+
+	public int getEstadoId() {
+		return estadoId;
+	}
+
+	public void setEstadoId(Integer estadoId) {
+		this.estadoId = estadoId;
+	}
+
+	@Override
+	public String toString() {
+		return "Municipio [municipioId=" + municipioId + ", nombreMunicipio=" + nombreMunicipio + ", estadoId="
+				+ estadoId + "]";
+	}
+	
+}
